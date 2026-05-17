9754cbcd964a44531a29ea0cf20fea6468226e0f
Lilly <derpyduckalotl@gmail.com>
2026-05-17 23:26:39 +0200

    change task1

--- task1/api.php
+++ task1/api.php
@@ -7,7 +7,6 @@ ini_set('display_errors', 1);
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
 
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {

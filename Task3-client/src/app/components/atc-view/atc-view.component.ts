d2abdd099e0005d7caecb23eda96c90f3f1d7b2d
Lilly <derpyduckalotl@gmail.com>
2026-05-17 18:38:21 +0200

    Leaflet map

--- Task3-client/src/app/components/atc-view/atc-view.component.ts
+++ Task3-client/src/app/components/atc-view/atc-view.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { WebsocketService } from '../../services/websocket.service';
 
 @Component({
   selector: 'app-atc-view',
   templateUrl: './atc-view.component.html',
-  styleUrl: './atc-view.component.css'
+  styleUrls: ['./atc-view.component.css']
 })
-export class AtcViewComponent {
+export class AtcViewComponent implements OnInit, OnDestroy {
 
+  userName: string = localStorage.getItem('userName') || 'ATC';
+
+  // Mock flights - will come from API later
+  flights: any[] = [
+    { id: 1, flight_number: 'SA203', origin_code: 'JNB', destination_code: 'CPT', status: 'Scheduled', flight_duration_hours: 2, passengers: [] },
+    { id: 2, flight_number: 'SA101', origin_code: 'CPT', destination_code: 'DUR', status: 'Scheduled', flight_duration_hours: 1.5, passengers: [] },
+    { id: 3, flight_number: 'SA305', origin_code: 'DUR', destination_code: 'JNB', status: 'In Flight', flight_duration_hours: 1, passengers: [] }
+  ];
+
+  selectedFlight: any = null;
+  isConnected: boolean = false;
+  connectionError: string = '';
+  notifications: string[] = [];
+
+  private messageSub: Subscription | null = null;
+  private statusSub: Subscription | null = null;
+
+  constructor(private wsService: WebsocketService) {}
+
+  ngOnInit() {
+    this.statusSub = this.wsService.connectionStatus$.subscribe(status => {
+      this.isConnected = status;
+      if (!status) {
+        this.connectionError = 'Disconnected from server. Please refresh.';
+      } else {
+        this.connectionError = '';
+      }
+    });
+
+    this.messageSub = this.wsService.messages$.subscribe(message => {
+      this.handleMessage(message);
+    });
+  }
+
+  handleMessage(message: any) {
+    switch(message.type) {
+      case 'BOARD':
+        this.notifications.unshift(`Passenger confirmed boarding on flight ${message.flight_id}`);
+        break;
+      case 'POSITION':
+        console.log('Position update:', message);
+        break;
+      case 'SHUTDOWN':
+        this.connectionError = 'Server is shutting down.';
+        break;
+    }
+  }
+
+  selectFlight(flight: any) {
+    this.selectedFlight = flight;
+  }
+
+  dispatchFlight(flight: any) {
+    this.wsService.send({
+      type: 'DISPATCH',
+      flight_id: flight.id,
+      apikey: localStorage.getItem('apikey')
+    });
+    
+    flight.status = 'Boarding';
+    this.notifications.unshift(`Dispatched flight ${flight.flight_number}`);
+  }
+
+  trackFlight(flightId: number) {
+    this.wsService.send({
+      type: 'TRACK',
+      flight_id: flightId,
+      apikey: localStorage.getItem('apikey')
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.messageSub) this.messageSub.unsubscribe();
+    if (this.statusSub) this.statusSub.unsubscribe();
+  }
 }
+
--- Task3-client/src/app/components/map/map.component.ts
+++ Task3-client/src/app/components/map/map.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, SimpleChanges, OnChanges } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements OnInit, OnDestroy, OnChanges {
 
+  @Input() airports: any[] = [];
+  @Input() flightPosition: any = null;
+
+  private map: L.Map | null = null;
+  private airportMarkers: L.Marker[] = [];
+  private aircraftMarker: L.Marker | null = null;
+
+  private airportIcon = L.icon({
+    iconUrl: 'assets/airport.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32]
+  });
+
+  private aircraftIcon = L.icon({
+    iconUrl: 'assets/plane.png',
+    iconSize: [40, 40],
+    iconAnchor: [20, 20],
+    popupAnchor: [0, -20]
+  });
+
+  private mockAirports = [
+    { name: 'OR Tambo', iata_code: 'JNB', latitude: -26.1392, longitude: 28.2460 },
+    { name: 'Cape Town International', iata_code: 'CPT', latitude: -33.9648, longitude: 18.6017 },
+    { name: 'King Shaka', iata_code: 'DUR', latitude: -29.6144, longitude: 31.1197 },
+    { name: 'Heathrow', iata_code: 'LHR', latitude: 51.4700, longitude: -0.4543 },
+    { name: 'JFK', iata_code: 'JFK', latitude: 40.6413, longitude: -73.7781 }
+  ];
+
+  ngOnInit() {
+    this.initMap();
+    this.plotAirports(this.mockAirports);
+  }
+
+  private initMap() {
+    this.map = L.map('map', {
+      center: [0, 20],
+      zoom: 3
+    });
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
+
+  private plotAirports(airports: any[]) {
+    if (!this.map) return;
+
+    airports.forEach(airport => {
+      const marker = L.marker(
+        [airport.latitude, airport.longitude],
+        { icon: this.airportIcon }
+      )
+      .addTo(this.map!)
+      .bindPopup(`<b>${airport.iata_code}</b><br>${airport.name}`);
+      this.airportMarkers.push(marker);
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['flightPosition'] && this.map) {
+      this.updateAircraftPosition(changes['flightPosition'].currentValue);
+    }
+    if (changes['airports'] && this.airports.length > 0) {
+      this.plotAirports(this.airports);
+    }
+  }
+
+  updateAircraftPosition(position: any) {
+    if (!this.map || !position) return;
+
+    if (!this.aircraftMarker) {
+      this.aircraftMarker = L.marker(
+        [position.latitude, position.longitude],
+        { icon: this.aircraftIcon }
+      ).addTo(this.map);
+    } else {
+      this.aircraftMarker.setLatLng([position.latitude, position.longitude]);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.map) {
+      this.map.remove();
+    }
+  }
 }
+

68ed7cb160a54cc91ffa286e3528a841bf5708ce
Lilly <derpyduckalotl@gmail.com>
2026-05-17 23:27:09 +0200

    authorization change for api

--- task1/api.php
+++ task1/api.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
     http_response_code(200);
